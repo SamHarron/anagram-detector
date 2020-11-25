f1e9a625b9558d7949c87f1e18c33620ba03b9c1
SamHarron <harronsam@gmail.com>
2020-11-25 12:24:11 -0500

    commit tree 10a0117d64d669417eae072bded846a29c779b8b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_of_words)
+        array_of_words.select do |letters| 
+            (@word.split("").sort == letters.split("").sort)
+    end
+end
+end
+
+listen = Anagram.new("listen")
+puts listen.match(%w(enlists google inlets silent))
+
+angel = Anagram.new("angel")
+puts angel.match(%w(lane cup glean fool))
+
+study = Anagram.new("study")
+puts study.match(%w(dusty inch rat sister))
+
+
+
